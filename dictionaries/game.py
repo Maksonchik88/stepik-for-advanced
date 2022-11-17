42b97c36853718dff018c95a109a4337e62a533e
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-11-17 22:42:01 +0300

    Finished game

--- dictionaries/game.py
+++ dictionaries/game.py
@@ -7,34 +7,49 @@ win_list = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1
 
 def field_to_play():
     for el in range(3):
-        print(game_area[0 + el * 3],  game_area[1 + el * 3],  game_area[2 + el * 3])
-
+        print(game_area[0 + el * 3], game_area[1 + el * 3], game_area[2 + el * 3])
 
 
 def cross_or_toe(token):
     while True:
-        elem = print("Select input field:" + token)
-        if elem in '123456789':
-            elem = int(elem)
-            if str(game_area [elem - 1] in "XO"):
-                print("The field is occupied =(. Try again =)")
-                continue
-            else:
-                game_area[elem - 1] = token
-                break
-        else:
-            print("Wrong! Try again =)")
+        elem = input("Select input field:" + token + "?")
+        if not (elem in '123456789'):
+            print("Error. Try again =)")
+            continue
+        elem = int(elem)
+        if str(win_list[elem - 1]) in "XO":
+            print("The field is occupied =(. Try again =)")
             continue
 
 
-
-
 def program_chek():
-    for i in win_list:
-        for el in i:
-            if win_list[i][el] == win_list[i][el + 1] == win_list[i][el + 2]:
-
+    for el in win_list:
+        if win_list[el[0] - 1] == win_list[el[1] - 1 == win_list[el[2] - 1]]:
+            return win_list[el[1] - 1]
+    else:
+        return False
 
 
 def main():
-    pass
+    counter = 0
+    while True:
+        field_to_play()
+        if counter % 2 == 0:
+            cross_or_toe('X')
+        else:
+            cross_or_toe('O')
+            if counter > 3:
+                winner = program_chek()
+                if winner:
+                    field_to_play()
+                    print(winner, "win!")
+                    break
+            else:
+                counter += 1
+                if counter > 8:
+                    field_to_play()
+                    print("Draw =)")
+                    break
+
+
+main()
