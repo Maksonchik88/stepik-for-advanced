96fd759109691d28bfc0937a8c0f73d4e9191a17
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-11-23 18:31:53 +0300

    new var

--- dictionaries/synonym_dictionary.py
+++ dictionaries/synonym_dictionary.py
@@ -2,11 +2,8 @@ res = {}
 
 for i in range(int(input())):
     key, value = input().split()
-    res[key], res[value] = value, key    
+    res[key], res[value] = value, key
 
-key_word = input()
-
-if key_word not in res:
-    print(None)
-else:
-    print(res[key_word])
+word = input()
+syn = res.get(word)
+print(syn)
