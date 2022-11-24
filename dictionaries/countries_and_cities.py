a2349177d15248d219c66c55c432ab4dfca9105e
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-11-24 19:11:34 +0300

    ex_5

---
+++ dictionaries/countries_and_cities.py
@@ -0,0 +1,18 @@
+n = int(input())
+general_dict = dict()
+for i in range(n):
+    my_list = input().split()
+    val_list = my_list[0]
+    key_list = my_list[1:]
+    new_dict = dict.fromkeys(key_list, val_list)
+    general_dict.update(new_dict)
+
+m = int(input())
+general_list = []
+for j in range(m):
+    test = input()
+    general_list.append(general_dict[test])
+
+for el in general_list:
+    print(el)
+
