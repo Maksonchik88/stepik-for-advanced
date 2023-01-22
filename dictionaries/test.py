7e2979f21d33d4c943629555e94558daf4ff442b
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-22 14:00:48 +0300

    easy

--- dictionaries/test.py
+++ dictionaries/test.py
@@ -1,8 +1,23 @@
-list_digit = [4, 9, 5, 1, 8 ]
-print(list_digit)
+data = {}
+for line in range(int(input())):
+    buyer, goods, number = input().split()
+    if buyer not in data:
+        data[buyer] = {goods: int(number)}
+    else:
+        if goods not in data[buyer]:
+            data[buyer][goods] = int(number)
+        else:
+            data[buyer][goods] += int(number)
 
-print("Сумма цифр", sum(list_digit))
-print("Количество цифр", len(list_digit))
-print("Минимальная цифра", min(list_digit))
-print("Максимальная цифра", max(list_digit))
-print("Среднее арифметическое цифр", sum(list_digit)/len(list_digit))
+data_sort = sorted(data.items(), key=lambda x: x[0])
+
+for name, goods_buyer in data_sort:
+    print(name, ':', sep='')
+    goods_buyer_sort = sorted(goods_buyer.items(), key=lambda x: x[0])
+    for goods, buyer in goods_buyer_sort:
+        print(goods, buyer)
+
+
+
+
+
