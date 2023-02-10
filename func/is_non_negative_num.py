3969206cb330646aee21a8a18cdcade148558613
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-02-10 11:27:30 +0300

    https://stepik.org/lesson/512854/step/7?unit=505068

---
+++ func/is_non_negative_num.py
@@ -0,0 +1,11 @@
+is_non_negative_num = lambda st: not st.isalpha() and not st.count('.') > 1 and not st[0] == '-' and st.upper() == st.lower()
+
+
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))
